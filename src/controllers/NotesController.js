eb72a48089c09ce10a7ae5b36c7496564dfd5851
joao <pjoaovictor22@hotmail.com>
2022-09-20 01:43:44 -0300

    Atualizado

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -71,23 +71,29 @@ class NotesController{
             //tags.split(',') = converte esse texto em array utilizando como a delimitador a virgula
             //map(tag =>tag.trim()) =pegar só a tag 
             //O método trim() remove os espaços em branco (whitespaces) do início e/ou fim de um texto
-            const filterTags = tags.split(',').map(tag =>tag.trim()); 
+            const filterTags = tags.split(',').map(tag => tag.trim()); 
              
-          
-            notes = await knex("tags").select([
+            //conectando a tabela de tags e notas
+            notes = await knex("tags")
+            .select([
                 "notes.id",
                 "notes.title",
-                "notes.user_id"
+                "notes.user_id",
             ])
-            .where("note.user_id","=", user_id)
+            .where("note.user_id","=", user_id) //filtrar baseado no id do usuário
+            .whereLike("notes.title", `%${title}%`)
             .whereIn("name", filterTags)   //analisa baseado na tag e passa o vetor que quer que compare se a tag existe
+            .innerJoin("notes","notes.id","tags.notes_id")
+            .orderBy("notes.title")
         }else{
-            notes = await knex("notes").where({user_id}).whereLike("title" , `%${title}%`).orderBy("title");
+            notes = await knex("notes")
+            .where({user_id})
+            .whereLike("title" , `%${title}%`)
+            .orderBy("title");
         }
 
         return response.json(notes);
     }
-    //rever a aula Aplicando Inner Join pois não entendi foi nada
 }
 
 module.exports = NotesController;
